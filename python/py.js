5592938744c227454e1bae229f3a2e45724b0541
technestx <prashant2123@outlook.com>
2025-10-12 23:06:52 +0530

    Add files via upload

---
+++ python/py.js
@@ -0,0 +1,159 @@
+const dataModel = [
+    {
+        title:"Intro",
+        url : "py_intro.html"
+    },
+    {
+        title:"Getting Started",
+        url : "py_getting_started.html"
+    },
+    {
+        title:"Python Syntex",
+        url : "py_syntex.html"
+    },
+    {
+        title:"Python Comments",
+        url : "py_comments.html"
+    },
+    {
+        title:"Python Variables",
+        url : "py_variables.html"
+    },
+    {
+        title:"Python Data Types",
+        url : "py_datatypes.html"
+    },
+    {
+        title:"Python Numbers",
+        url: "py_numbers.html"
+    },
+    {
+        title:"Python Casting",
+        url : "py_casting.html"
+    },
+    {
+        title:"Python String",
+        url : "py_string.html"
+    },
+    {
+        title:"Python Boolean",
+        url : "py_boolean.html"
+    },
+    {
+        title:"Python Operators",
+        url : "py_operators.html"
+    },
+    {
+        title:"Python List",
+        url : "py_lists.html"
+    },
+    {
+        title:"Python Tuples",
+        url : "py_tuples.html"
+    },
+    {
+        title:"Python Set",
+        url : "py_set.html"
+    },
+    {
+        title: "Python Dictionary",
+        url : "py_dict.html"
+    },
+    {
+        title:"Python If...Else",
+        url : "py_if_else.html"
+    },
+    {
+        title:"Python While Loop",
+        url : "py_while.html"
+    },
+    {
+        title: "Python For Loop",
+        url: "py_for.html"
+    },
+    {
+        title: "Python Function",
+        url : "py_function.html"
+    },
+    {
+        title : "Python Lambda",
+        url : "py_lambda.html"
+    },
+    {
+        title:"Python Arrays",
+        url : "py_arrays.html"
+    },
+    {
+        title:"Python Classes and Objects",
+        url : "py_classes.html"
+    },
+    {
+        title : "Python Inheritance",
+        url : "py_inheritance.html"
+    },
+    {
+        title : "Python Iterator",
+        url : "py_iterator.html"
+    },
+    {
+        title : "Python Polymorphism",
+        url : "py_posimorphism.html"
+    },
+    {
+        title : "Python Scope",
+        url : "py_scope.html"
+    },
+    {
+        title : "Python Module",
+        url : "py_module.html"
+    },
+    {
+        title : "Python Date",
+        url : "py_date.html"
+    },
+    {
+        title : "Python Math",
+        url : "py_math.html"
+    },
+    {
+        title : "Python Json",
+        url : "py_json.html"
+    },
+    {
+        title : "Python Regex",
+        url : "py_regex.html"
+    },
+    {
+        title : "Python PIP",
+        url : "py_pip.html"
+    },
+    {
+        title : "Python Try Except",
+        url : "py_try.html"
+    },
+    {
+        title : "Python User Input",
+        url : "py_input.html"
+    },
+    {
+        title : "Python String Formatting",
+        url : "py_formatting.html"
+    },
+    {
+        title : "Python File Handling",
+        url : "py_file_handling.html"
+    },
+    {
+        title : "Python Read File",
+        url : "py_read_file.html"
+    },
+    {
+        title : "Python Write File",
+        url : "py_write_file.html"
+    },
+    {
+        title : "Python Delete File",
+        url : "py_delete_file.html"
+    }
+
+]

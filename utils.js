079522b2ca6f85add0816c55312f862d9e142987
technestx <prashant2123@outlook.com>
2025-10-13 14:03:31 +0530

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,6 @@
+const bloglinks = "Recent Posts<br> <a href='../blog/stream-in-nodejs.html'>Stream in NodeJS</a><br>"
++"<a href='../blog/duration-object-in-wpf.html'>Duration Object in WPF</a>";
+
+function getHostName(){
+    return window.location.protocol+"//"+window.location.hostname+"/";
+}

1edab64b89d0d2f896873007b8587bb5c868bbb0
technestx <prashant2123@outlook.com>
2025-10-14 19:22:52 +0530

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 const bloglinks = "Recent Posts<br> <a href='../blog/stream-in-nodejs.html'>Stream in NodeJS</a><br>"
-+"<a href='../blog/duration-object-in-wpf.html'>Duration Object in WPF</a>";
++"<a href='../blog/duration-object-in-wpf.html'>Duration Object in WPF</a><br>" +
+    "<a href='../blog/how-to-ceo-website.html'>How to CEO your website</a><br>";
 
 function getHostName(){
     return window.location.protocol+"//"+window.location.hostname+"/";

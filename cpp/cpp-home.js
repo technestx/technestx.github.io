19a33bebfd4e31094d271cea667519a5d2ccc527
technestx <prashant2123@outlook.com>
2025-11-30 22:44:17 +0530

    Add files via upload

---
+++ cpp/cpp-home.js
@@ -0,0 +1,142 @@
+const dataModel = [
+    {
+        title:"Introduction To C++",
+        url : "cpp_intro.html"
+    },
+    {
+        title:"C++ Development Environment",
+        url : "cpp_dev.html"
+    },
+    {
+        title:"C++ Basic",
+        url : "cpp_basics.html"
+    },
+    {
+        title:"C++ Syntax",
+        url : "cpp_syntax.html"
+    },
+    {
+        title:"C++ Output",
+        url : "cpp_output.html"
+    },
+    {
+        title:"C++ Comments",
+        url : "cpp_comments.html"
+    },
+    {
+        title:"C++ Variables",
+        url: "cpp_variables.html"
+    },
+    {
+        title:"C++ User Input",
+        url : "cpp_user_input.html"
+    },
+    {
+        title:"C++ Data Types",
+        url : "cpp_data_types.html"
+    },
+    {
+        title:"C++ Operators",
+        url : "cpp_operators.html"
+    },
+    {
+        title:"C++ String",
+        url : "cpp_string.html"
+    },
+    {
+        title:"C++ Math",
+        url : "cpp_math.html"
+    },
+    {
+        title:"C++ Booleans",
+        url : "cpp_booleans.html"
+    },
+    {
+        title:"C++ Conditions",
+        url : "cpp_conditions.html"
+    },
+    {
+        title: "C++ While/Do-While Loop",
+        url : "cpp_while.html"
+    },
+    {
+        title:"C++ For Loop",
+        url : "cpp_for.html"
+    },
+    {
+        title:"C++ Break and Continue",
+        url : "break.html"
+    },
+    {
+        title: "C++ Arrays",
+        url: "cpp_array.html"
+    },
+    {
+        title: "C++ Structures",
+        url : "cpp_struct.html"
+    },
+    {
+        title:"C++ References",
+        url : "cpp_ref.html"
+    },
+    {
+        title:"C++ Pointers",
+        url : "pointers.html"
+    },
+    {
+        title: "C++ Functions",
+        url: "cpp_functions.html"
+    },
+    {
+        title: "C++ function Parameters",
+        url : "cpp_fun_para.html"
+    },
+    {
+        title: "C++ Function Overloading",
+        url : "cpp_fun_over.html"
+    },
+    {
+        title: "C++ Recursion",
+        url : "cpp_recursion.html"
+    },
+    {
+        title: "C++ OOP",
+        url : "cpp_oop.html"
+    },
+    {
+        title: "C++ Classes and Objects",
+        url : "cpp_classes.html"
+    },
+    {
+        title: "C++ Class Methods",
+        url : "cpp_methods.html"
+    },
+    {
+        title: "C++ Cunstructors",
+        url : "cpp_const.html"
+    },
+    {
+        title: "C++ Access Specifiers",
+        url : "cpp_access.html"
+    },
+    {
+        title: "C++ Encapsulation",
+        url : "cpp_encap.html"
+    },
+    {
+        title: "C++ Inheritance",
+        url : "cpp_inher.html"
+    },
+    {
+        title: "C++ Polymorphism",
+        url : "cpp_poly.html"
+    },
+    {
+        title: "C++ Files",
+        url : "cpp_files.html"
+    },
+    {
+        title: "C++ Exceptions",
+        url : "cpp_excep.html"
+    }
+]
